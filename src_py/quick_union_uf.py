9e66df5ec81a7a9ff4cf2bbee5529af5175a6a62
Fynn7 <82013676+Fynn7@users.noreply.github.com>
2026-04-20 13:39:58 +0200

    Create quick_union_uf.py

---
+++ src_py/quick_union_uf.py
@@ -0,0 +1,18 @@
+from DSA import intArray
+
+
+class QuickUnionUF:
+    def __init__(self, n):
+        self.id = intArray(n)
+        for i in range(len(self.id)):
+            self.id[i] = i
+
+    def find(self, i):
+        while i != self.id[i]:
+            i = self.id[i]
+        return i
+
+    def union(self, p, q):
+        i = self.find(p)
+        j = self.find(q)
+        self.id[i] = j
